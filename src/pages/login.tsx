1fa623f7b975ff741d524a6fa0ddb6049617321d
dewanshDT <dewansh.dt@gmail.com>
2023-09-02 10:12:43 +0530

    login and signup

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,38 @@
-import { Button } from '../components'
+import { Button, Field } from '../components'
 
 const LoginPage = () => {
   return (
     <div className="flex flex-col justify-center items-center h-full w-full">
-      <div className="flex gap-4 flex-col">
-        <Button>Login</Button>
-        <Button intent="secondary">Sign up</Button>
+      <div className="w-full max-w-sm gap-4 flex flex-col">
+        <div className="gap-4">
+          <h3 className="font-bold text-2xl w-full text-neutral-black">
+            Sign In to WisdomCircle
+          </h3>
+          <p className="text-neutral-grey">
+            Don’t have an account?{' '}
+            <a href="/auth/signup" className="font-semibold">
+              Sign Up
+            </a>
+          </p>
+        </div>
+        <form className="flex gap-4 w-full flex-col">
+          <div className="gap-4 flex flex-col w-full">
+            <Field placeholder="Email Address" required type="email" />
+
+            <div className="flex flex-col gap-2 w-full">
+              <Field
+                required
+                placeholder="Password"
+                autoComplete="current-password"
+                type="password"
+              />
+              <a href="/forgot" className="font-semibold ml-auto">
+                Forgot Password
+              </a>
+            </div>
+          </div>
+          <Button>Sign Up</Button>
+        </form>
       </div>
     </div>
   )
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,22 +1,52 @@
+import PhoneInput from 'react-phone-input-2'
 import { Button, Field } from '../components'
+import { useState } from 'react'
 
 const SignupPage = () => {
+  const [value, setValue] = useState('')
   return (
     <div className="flex flex-col justify-center items-center h-full w-full">
-      <div className="min-w-[384px] gap-4 flex flex-col">
-        <div className="gap-2">
+      <div className="w-full max-w-sm gap-4 flex flex-col">
+        <div className="gap-4">
           <h3 className="font-bold text-2xl w-full text-neutral-black">
             Create an account
           </h3>
-          <p className="text-neutral-grey">Already have an account? Sign In</p>
+          <p className="text-neutral-grey">
+            Already have an account?{' '}
+            <a href="/auth/login" className="font-semibold">
+              Sign In
+            </a>
+          </p>
         </div>
         <form className="flex gap-4 w-full flex-col">
           <div className="gap-4 flex flex-col w-full">
-            <Field placeholder="First Name" />
-            <Field placeholder="Last Name" />
-            <Field placeholder="Email Address" type="email" />
-            <Field placeholder="Password" type="password" />
+            <Field placeholder="First Name" required />
+            <Field placeholder="Last Name" required />
+            <Field placeholder="Email Address" required type="email" />
+            <PhoneInput
+              country="in"
+              value={value}
+              onChange={(value) => {
+                setValue(value as string)
+              }}
+            />
+            <div className="flex flex-col gap-2 w-full">
+              <Field
+                required
+                placeholder="Password"
+                autoComplete="new-password"
+                type="password"
+              />
+              <div className="text-xs text-neutral-grey">
+                Password must be at least 8 characters
+              </div>
+            </div>
           </div>
+          <p className="text-neutral-grey text-xs my-2">
+            By clicking Sign Up you are indicating that you have read and
+            acknowledged the <a href="/terms">Terms of Service</a> and{' '}
+            <a href="/privacy">Privacy Notice</a>
+          </p>
           <Button>Sign Up</Button>
         </form>
       </div>
