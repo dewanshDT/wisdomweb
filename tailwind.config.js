70abe45ddeae587b08fcdf465ceb017c486a3629
dewanshDT <dewansh.dt@gmail.com>
2023-09-02 07:06:04 +0530

    added colors and fonts to the tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-100': '#FEFCF4',
+        'primary-400': '#F1C12B',
+        'primary-600': '#A9871E',
+        'neutral-black': '#121317',
+        'neutral-grey': '#404555',
+        'neutral-divider': '#DCDEE5',
+        'neutral-charcoal': '#606880',
+      },
+      fontFamily: {
+        Poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
