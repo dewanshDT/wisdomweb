cae613189ae808b6139e9329ba6441b273dab7f2
dewanshDT <dewansh.dt@gmail.com>
2023-09-02 08:10:47 +0530

    modified the field to use password and ref

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,52 +1,92 @@
-import { cva, type VariantProps } from 'class-variance-authority'
+import { forwardRef, HTMLProps, ForwardedRef, useState } from 'react'
 import clsx from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-const input = cva(
-  clsx(
+// Define your variant configuration here
+const inputVariants = {
+  intent: {
+    primary: ['border', 'rounded-sm', 'focus:border-blue-600'],
+    secondary: [
+      'bg-transparent',
+      'border-b-2',
+      'focus:border-blue-600',
+      'px-[.5em] py-[.2em]',
+    ],
+  },
+  inputSize: {
+    small: ['text-sm', 'py-1.5', 'px-2'],
+    base: ['text-base', 'py-2.5', 'px-4'],
+    medium: ['text-lg', 'py-3', 'px-5'],
+  },
+}
+
+// Create a function that generates the class variance authority
+const createInputCVA = () => {
+  return clsx(
+    'relative',
     'bg-white',
     'placeholder:text-neutral-charcoal',
     'border-neutral-divider',
     'focus:outline-none',
-  ),
-  {
-    variants: {
-      intent: {
-        primary: ['border', 'rounded-sm', 'focus:border-primary-600'],
-        secondary: [
-          'bg-transparent',
-          'border-b-2',
-          'focus:border-primary-400',
-          'px-[.5em] py-[.2em]',
-        ],
-      },
-      inputSize: {
-        small: ['text-sm', 'py-1.5', 'px-2'],
-        base: ['text-base', 'py-2.5', 'px-4'],
-        medium: ['text-lg', 'py-3', 'px-5'],
-      },
-    },
-    defaultVariants: {
-      intent: 'primary',
-      inputSize: 'base',
-    },
+    'w-full',
+  )
+}
+
+// ForwardRef callback type
+type InputRefCallback = (instance: HTMLInputElement | null) => void
+
+// InputProps with ref forwarding support
+interface InputProps extends Omit<HTMLProps<HTMLInputElement>, 'className'> {
+  className?: string
+  intent?: keyof typeof inputVariants.intent
+  inputSize?: keyof typeof inputVariants.inputSize
+}
+
+const Field = forwardRef<InputRefCallback, InputProps>(
+  (
+    { className, intent = 'primary', inputSize = 'base', type, ...props },
+    ref,
+  ) => {
+    const [showPassword, setShowPassword] = useState(false)
+
+    const inputType = type === 'password' && showPassword ? 'text' : type
+
+    const inputClass = twMerge(
+      createInputCVA(),
+      clsx(inputVariants.intent[intent]),
+      clsx(inputVariants.inputSize[inputSize]),
+      className,
+    )
+
+    if (type !== 'password')
+      return (
+        <input
+          className={inputClass}
+          {...props}
+          ref={ref as ForwardedRef<HTMLInputElement>}
+        />
+      )
+
+    return (
+      <div className="relative w-full">
+        <input
+          type={inputType}
+          className={inputClass}
+          {...props}
+          ref={ref as ForwardedRef<HTMLInputElement>}
+        />
+        {type === 'password' && (
+          <button
+            type="button"
+            className="absolute top-1/2 -translate-y-1/2 right-2 text-gray-400 hover:text-gray-600 cursor-pointer"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? '🙈' : '👁️'}
+          </button>
+        )}
+      </div>
+    )
   },
-)
-
-interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement>,
-    VariantProps<typeof input> {}
-
-const Field: React.FC<InputProps> = ({
-  className,
-  intent,
-  inputSize,
-  ...props
-}) => (
-  <input
-    className={twMerge(input({ intent, inputSize }), className)}
-    {...props}
-  />
 )
 
 export default Field
