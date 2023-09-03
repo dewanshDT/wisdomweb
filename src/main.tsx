b91e4fe545eda621a65e83aef4f19f8a9d4db5c6
dewanshDT <dewansh.dt@gmail.com>
2023-09-04 02:24:53 +0530

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -5,28 +5,31 @@ import './index.css'
 import { AuthLayout, RootLayout } from './components'
 import { ErrorPage, HomePage, LoginPage, SignupPage } from './pages'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-    ],
-  },
-  {
-    path: '/auth',
-    element: <AuthLayout />,
-    children: [
-      { index: true, element: <Navigate replace to="/auth/login" /> },
-      { path: 'signup', element: <SignupPage /> },
-      { path: 'login', element: <LoginPage /> },
-    ],
-  },
-])
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <RootLayout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+      ],
+    },
+    {
+      path: '/auth',
+      element: <AuthLayout />,
+      children: [
+        { index: true, element: <Navigate replace to="/auth/login" /> },
+        { path: 'signup', element: <SignupPage /> },
+        { path: 'login', element: <LoginPage /> },
+      ],
+    },
+  ],
+  // { basename: import.meta.env.DEV ? '/' : '/react-vite-gh-pages/' },
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
