2c515a6028a804e5e6fc96c4628f5d42c776a5cc
dewanshDT <dewansh.dt@gmail.com>
2023-09-04 02:36:50 +0530

    base

--- src/main.tsx
+++ src/main.tsx
@@ -28,7 +28,7 @@ const router = createBrowserRouter(
       ],
     },
   ],
-  // { basename: import.meta.env.DEV ? '/' : '/react-vite-gh-pages/' },
+  { basename: import.meta.env.DEV ? '/' : '/react-vite-gh-pages/' },
 )
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
